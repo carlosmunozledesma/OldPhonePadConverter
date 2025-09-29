401a9c88d668392001f88f5b6c6ae3b78106e735
Carlos Munoz Ledesma <Carlos@tfo.group>
2025-09-29 12:59:54 +0100

    Refactor Program class

--- OldPhonePadConverter/Program.cs
+++ OldPhonePadConverter/Program.cs
@@ -5,29 +5,50 @@ namespace OldPhonePadConverter
     {
         public static void Main()
         {
+            ShowGreedings();
+            string message = GetMessageFromUser();
+            while (!string.IsNullOrEmpty(message) && !IsMessageToExit(message))
+            {
+                var result = OldPhonePadConverter.OldPhonePad(message);
+
+                ShowResult(result);
+
+                message = GetMessageFromUser();
+
+            }
+        }
+
+        private static void ShowGreedings()
+        {
+            Console.WriteLine("-------------------------------------------");
             Console.WriteLine("----- Old Phone Pad Converter to Text -----");
+            Console.WriteLine("-------------------------------------------");
 
             Console.WriteLine("Write exit to stop the program.");
-            Console.WriteLine("Enter a message to convert:");
+        }
+
+        private static string GetMessageFromUser()
+        {
+            Console.WriteLine("Enter a message to convert: ");
             string? message = Console.ReadLine();
-
-            while (!String.Equals(message, Constants.Keypads.Exit,
-                   StringComparison.OrdinalIgnoreCase))
+            while (string.IsNullOrEmpty(message) || !message.Contains(Constants.Keypads.LastCharacterSentinel))
             {
-                while (string.IsNullOrEmpty(message) || !message.Contains(Constants.Keypads.LastCharacterSentinel))
-                {
-                    Console.WriteLine("No message entered or incompleted, add # at the end.");
-                    Console.WriteLine("Enter a message to convert:");
-                    message = Console.ReadLine();
-                }
-
-                var result = OldPhonePadConverter.OldPhonePad(message);
-                Console.WriteLine("Old Phone Pad output:" + result);
-
-                Console.WriteLine("Enter another message to convert:");
+                Console.WriteLine("No message entered or incompleted, add # at the end.");
+                Console.WriteLine("Enter a message to convert:");
                 message = Console.ReadLine();
             }
+            return message;
+        }
 
+        private static bool IsMessageToExit(string? message)
+        {
+            return String.Equals(message, Constants.Keypads.Exit,
+                   StringComparison.OrdinalIgnoreCase);
+        }
+
+        private static void ShowResult(string result)
+        {
+            Console.WriteLine("Old Phone Pad output: " + result);
         }
     }
 }
